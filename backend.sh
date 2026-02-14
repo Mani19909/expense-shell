ba1057687c915bd0aa66b2be91491f4b514f435c
manikanta Routhu <manikantar129@gmail.com>
2026-02-14 17:07:25 +0530

    expense-project

---
+++ backend.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+echo "please enter DB password:"
+read -s mysql_root_password
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+    echo -e "$2....$R FAILURE $N"
+    else
+    echo -e "$2....$G SUCCESS $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+echo "Please run this script with root access."
+exit 1
+else
+echo "you are in super user."
+fi
+
+dnf module disable nodejs -y &>>$LOGFILE
+VALIDATE $? "Disable default nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOGFILE
+VALIDATE $? "Enable nodejs:20"
+
+dnf install nodejs -y &>>$LOGFILE
+VALIDATE $? "Install nodejs"
+
+id expense &>>$LOGFILE
+if [ $? -ne 0 ]
+then 
+useradd expense &>>$LOGFILE
+VALIDATE $? "Creating expense user"
+else
+echo -e "Expense user already created...$Y SKIPPING $N"
+fi
+
+mkdir -p /app &>>$LOGFILE
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE
+VALIDATE $? "Downloading backend code"
+
+cd /app 
+unzip /tmp/backend.zip &>>$LOGFILE
+VALIDATE $? "Extracted backend code"
+
+
+npm install &>>$LOGFILE
+VALIDATE $? "Installing nodejs dependencies"
+
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+VALIDATE $? "Copied backend service"
+
+systemctl daemon-reload &>>$LOGFILE
+VALIDATE $? "demon Reload"
+
+systemctl start backend &>>$LOGFILE
+systemctl enable backend
+VALIDATE $? "starting and enable backend"
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing mysql client"
+
+mysql -h db.daws78s.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+VALIDATE $? "Schema loading"
+
+systemctl restart backend &>>$LOGFILE
+VALIDATE $? "Restarting Backend"
